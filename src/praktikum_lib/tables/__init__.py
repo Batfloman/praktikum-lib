4d64b2743a581f48682fa256371c5b67f29bfb22
Florian Hirche <flhirche+github@gmail.com>
2025-07-22 00:25:04 +0200

    update __init__.py

--- src/praktikum_lib/tables/__init__.py
+++ src/praktikum_lib/tables/__init__.py
@@ -1,5 +1,13 @@
 from .latex_table import save_table as export_as_latex;
 from .csv_table import load_csv, load_csv_consts, load_csv_datacluster
-from .tables import *;
+from .cassy_table import load_cassy
+from .excel_table import read as load_excel
 
-__all__ = ["export_as_latex", "load_csv", "load_csv_consts", "load_csv_datacluster"]
+__all__ = [
+    "export_as_latex",
+    "load_csv",
+    "load_csv_consts",
+    "load_csv_datacluster",
+    "load_cassy",
+    "load_excel"
+]
