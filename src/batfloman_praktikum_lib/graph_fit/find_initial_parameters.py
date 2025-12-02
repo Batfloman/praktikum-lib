1b2d819f20a5840b63a4b31c772bae74b89f78b6
Batfloman <flhirche+github@gmail.com>
2025-12-02 18:44:58 +0100

    find_initial_parameters workflow

--- src/batfloman_praktikum_lib/graph_fit/find_initial_parameters.py
+++ src/batfloman_praktikum_lib/graph_fit/find_initial_parameters.py
@@ -1,26 +1,153 @@
-from dataclasses import dataclass, asdict
-from typing import Optional, Any, Union, List, Callable, TypedDict
-from PIL.Image import init
+from typing import Optional, Union, List, Callable
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button, TextBox
+from matplotlib.widgets import Slider, Button
 import numpy as np
 
 from pathlib import Path
 import json
 import inspect
-import time
 
 from ..tables.validation import ensure_extension
 from .fitModel import FitModel
 
-last_update_time = 0
+# ==================================================
 
-class InitialParameterType(TypedDict):
-    value: float
+class ParameterManager:
+    slider: Slider;
+
+    slider_value: float
+
+    center: float
+    min: float
+    max: float
+
+    def __init__(self,
+        param: str,
+        total_ax, 
+        initial_value: float,
+        update_callback,
+        min: Optional[float] = None,
+        max: Optional[float] = None,
+        center: Optional[float] = None,
+    ):
+        self.param = param;
+        self.center = center or initial_value
+        self.min = min or 0.5 * initial_value
+        self.max = max or 1.5 * initial_value
+
+        total_ax.set_xticks([])
+        total_ax.set_yticks([])
+        total_ax.set_frame_on(False)
+        from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+        slider_ax = inset_axes(
+            total_ax,
+            width="50%", height="60%",
+            loc='center left'
+        )
+        # Buttons rechts (je 10% Breite)
+        ax_mini = inset_axes(
+            total_ax,
+            bbox_to_anchor=(0.75, 0, 0.1, 1),
+            bbox_transform=total_ax.transAxes,
+            width="80%", height="80%",
+            # loc="center left"
+            loc="center"
+        )
+
+        ax_cent = inset_axes(
+            total_ax,
+            bbox_to_anchor=(0.85, 0, 0.1, 1),
+            bbox_transform=total_ax.transAxes,
+            width="80%", height="80%",
+            # loc="center left"
+            loc="center"
+        )
+
+        ax_expa = inset_axes(
+            total_ax,
+            bbox_to_anchor=(0.95, 0, 0.1, 1),
+            bbox_transform=total_ax.transAxes,
+            width="80%", height="80%",
+            loc="center"
+        )
+
+        self.slider_value = initial_value
+        self.slider = Slider(slider_ax, label=param, valmin=self.min, valmax=self.max, valinit=initial_value)
+
+        self.text_delta = total_ax.text(
+            0.65, 0.5, f"Δ = {self.max - self.min:.2f}", ha="left", va="center",
+            transform=total_ax.transAxes
+        )
+        self.b1 = Button(ax_mini, '–')
+        self.b2 = Button(ax_cent, '●')
+        self.b3 = Button(ax_expa, '+')
+
+        def slider_callback(val):
+            self.slider_value = val;
+            update_callback()
+
+        self.slider.on_changed(slider_callback)
+
+        def recreate_slider():
+            ax = self.slider.ax
+            label = self.slider.label.get_text()
+            val = self.slider.val
+            ax.clear()
+            self.slider = Slider(ax, 
+                 label=label, 
+                 valmin=self.min, 
+                 valmax=self.max,
+                 valinit=val,
+            )
+            self.slider.on_changed(slider_callback)  # reattach callback
+
+        def btn_center_callback(event):
+            offset = self.slider_value - self.center
+            self.min += offset
+            self.max += offset
+            self.center += offset
+            recreate_slider()
+
+        self.b2.on_clicked(btn_center_callback);
+
+        def btn_minimize_callback(event):
+            delta = abs(self.min - self.max)
+            new_delta = 0.5 * delta;
+
+            self.min = self.center - 0.5 * new_delta
+            self.max = self.center + 0.5 * new_delta
+
+            self.text_delta.set_text(f"Δ = {self.max - self.min:.2f}")
+            recreate_slider()
+
+        self.b1.on_clicked(btn_minimize_callback)
+
+        def btn_expand_callback(event):
+            delta = abs(self.min - self.max)
+            new_delta = 2 * delta;
+
+            self.min = self.center - 0.5 * new_delta
+            self.max = self.center + 0.5 * new_delta
+
+            self.text_delta.set_text(f"Δ = {self.max - self.min:.2f}")
+            recreate_slider()
+
+        self.b3.on_clicked(btn_expand_callback)
+
+    def get_value(self):
+        return self.slider_value
+
+    def to_dict(self):
+        return {
+            "min": self.min,
+            "max": self.max,
+            "center": self.center,
+            "slider_value": self.slider_value,
+        }
 
 def order_initial_params(
     model: Union[Callable, FitModel],
-    initial_params: Union[List[float], dict[str, float]],
+    initial_params: Union[List[float], dict[str, float], dict[str, ParameterManager]],
 ) -> List[float]:
     model_fn = model.model if isinstance(model, FitModel) else model
     # assume the first parameter is the continous
@@ -43,165 +170,72 @@ def order_initial_params(
                     f"Missing initial parameter: '{p}'. "
                     f"Expected parameters: {params}"
                 )
-            vals.append(initial_params[p])
+            val = initial_params[p]
+            if isinstance(val, ParameterManager):
+                vals.append(val.slider_value)
+            elif isinstance(val, dict):
+                vals.append(val.get("slider_value", 1.0))
+            else:
+                vals.append(val)
         return vals
     else:
         raise ValueError(
             "initial_params must be either a list of floats or a dict[str, float]."
         )
 
-# ==================================================
-# just types
-
-# @dataclass
-# class InitialParameter:
-#     value: float
-#     name: Optional[str] = None
-#
-#     @staticmethod
-#     def from_any(obj: Any) -> "InitialParameter":
-#         if isinstance(obj, InitialParameter):
-#             return obj
-#         if isinstance(obj, SliderSettings):
-#             return InitialParameter(
-#                 name = obj.name,
-#                 value = obj.value
-#             )
-#         if isinstance(obj, dict):
-#             return InitialParameter(
-#                 name = obj.get("name", None),
-#                 value = obj.get("value", 1.0),
-#             );
-#         if isinstance(obj, (int, float, np.integer, np.floating)):
-#             return InitialParameter(
-#                 value = float(obj),
-#             ) 
-#
-#         raise ValueError("")
-        
-
-@dataclass
-class SliderSettings:
-    slider_value: float
-    exponent: int = 0;
-    name: Optional[str] = None;
-
-    @property
-    def value(self):
-        return self.slider_value * 10**self.exponent
-
-    @value.setter
-    def value(self, new_val):
-        self.exponent = int(np.log10(new_val))
-        self.slider_value = int(new_val/10**self.exponent)
-
-    @staticmethod
-    def from_any(obj: Any) -> "SliderSettings":
-        """
-        Erstellt ein SliderSettings-Objekt aus:
-        - einem SliderSettings-Objekt (gibt es direkt zurück)
-        - einem dict mit keys 'slider_value' und 'exponent' oder 'exp'
-        """
-        if isinstance(obj, SliderSettings):
-            return obj
-        if isinstance(obj, dict):
-            # handle both possible keys
-            val = obj.get("value", None)
-            if val:
-                exp = int(np.log10(val))
-                slider_val = int(val/10**exp)
-            else:
-                exp = obj.get("exponent", obj.get("exp", 0))
-                slider_val = obj.get("slider_value", 1.0)
-            return SliderSettings(
-                name = obj.get("name", None),
-                exponent = exp,
-                slider_value = slider_val,
-            )
-        if isinstance(obj, InitialParameter):
-            exp = int(np.log10(obj.value))
-            val = int(obj.value / 10**exp)
-            return SliderSettings(
-                name = obj.name,
-                exponent=exp,
-                slider_value=val,
-            )
-        if isinstance(obj, (int, float, np.integer, np.floating)):
-            x = SliderSettings(0, 0)
-            x.value = float(obj)
-            return x;
-        raise TypeError(f"Cannot convert {obj} to SliderSettings")
 
 # ==================================================
+# saving & loading
 
-def _save_params(file: Path, params: dict[str, SliderSettings]):
-    """Speichert SliderSettings als JSON"""
+def _save_slider_settings(file: Path, params: dict[str, ParameterManager]):
     serializable = {}
     for k, v in params.items():
-        if isinstance(v, SliderSettings):
-            serializable[k] = asdict(v)
-        elif isinstance(v, dict):
-            serializable[k] = v
-        else:
-            raise TypeError(f"Unexpected value type for {k}: {type(v)}")
+        serializable[k] = v.to_dict()
     file.write_text(json.dumps(serializable, indent=2))
 
-def _load_params(file: Path, default: dict[str, SliderSettings]) -> dict[str, SliderSettings]:
-    """Lädt JSON und wandelt dicts in SliderSettings um"""
+def _load_slider_settings(file: Path) -> dict[str, dict]:
+    d = {};
     if not file.exists():
-        return default
+        return d;
 
-    raw = json.loads(file.read_text())
-    loaded = {}
-    for k, v in raw.items():
-        loaded[k] = SliderSettings.from_any(v)
-    # default values als Fallback ergänzen
-    for k, v in default.items():
-        if k not in loaded:
-            loaded[k] = v
-    return loaded
+    return json.loads(file.read_text())
 
 def _extract_default_values(
     x_data,
     y_data,
     model: Union[Callable, FitModel],
     user_specified: Optional[Union[List[float], dict[str, float]]] = None
-) -> dict[str, SliderSettings]:
+) -> dict[str, float]:
     params = list(inspect.signature(model).parameters.keys())[1:]
 
     # ensure all params have some kind of default
     default = { 
-        p: SliderSettings(
-            name = p,
-            slider_value=1,
-            exponent=0
-        ) for p in params
+        p: 1.0 for p in params 
     }
 
     from_guess = {} 
     if isinstance(model, FitModel):
         initial_guess = model.get_initial_guess(x_data, y_data);
         from_guess = { 
-            p: SliderSettings.from_any(initial_guess[i]) for i, p in enumerate(params)
+            p: v for p, v in zip(params, initial_guess)
         };
         default = {**default, **from_guess}
 
     if not user_specified:
         return default
 
+    if isinstance(user_specified, dict):
+        return {**default, **user_specified}
     if isinstance(user_specified, List):
-        from_values = {
-            p: SliderSettings.from_any(user_specified[i]) for i, p in enumerate(params)
-        }
+        if len(params) != len(user_specified):
+            raise ValueError("")
 
-    if isinstance(default_values, List):
-        from_values = {p : default_values[i] for i, p in enumerate(params)}
-        return {**std_default, **from_guess}
-        default = {**default, **from_values}
-    elif isinstance(default_values, dict):
-        default = {**default, **default_values}
-    else:
-        raise ValueError("")
+        from_values = {
+            p: user_specified[i] for i, p in enumerate(params)
+        }
+        return {**default, **from_values}
+
+# ==================================================
 
 def find_init_params(
     x_data,
@@ -217,125 +251,62 @@ def find_init_params(
     ax.set_title("Find Parameters")
     ax.scatter(x_data, y_data)
 
+    # assume first parameter is the continous parameter (e.g. `x` in `f(x)`)
     params = list(inspect.signature(model).parameters.keys())[1:]
-    
-    default = _extract_default_values(x_data, y_data, model, default_values)
-    slider_settings: dict[str, SliderSettings] = _load_params(CACHE, default=default) 
 
     sliderheight = 0.05
     offset = sliderheight * len(params) + 2 * sliderheight
     fig.subplots_adjust(bottom=offset)
+    
+    default = _extract_default_values(x_data, y_data, model, default_values)
+    default_vals: List[float] = order_initial_params(model, default);
+    cached = _load_slider_settings(CACHE)
+
+    cached_values = {};
+    for p in params:
+        if p in cached:
+            settings = cached[p]
+            val = settings.get("slider_value", None);
+            if val:
+                cached_values[p] = val
+
+    starting_params = order_initial_params(model, {**default, **cached_values})
 
     x_plot = np.linspace(x_data.min(), x_data.max(), 200)
-    line, = ax.plot(x_plot, model(x_plot, *[slider_settings[p].value for p in params]), color='red')
+    line, = ax.plot(x_plot, model(x_plot, *starting_params), color='red')
 
-    # since garbage collector would remove the references we store them
-    sliders = {}
-    btns_plus = {}
-    btns_minus = {}
-    exp_texts = {}
-    textboxes = {}
+    parameters: dict[str, ParameterManager] = {};
 
-    last_update_time = 0
-
-    def update_graph(param):
-        # global last_update_time
-        # # Debounce: mindestens 0.05s Abstand zwischen Redraws
-        # now = time.time()
-        # if now - last_update_time < 0.05:
-        #     return
-        # last_update_time = now
-
-        y_plot = model(x_plot, *[slider_settings[p].value for p in params])
-        line.set_ydata(y_plot)
-
-        paramval = slider_settings[p].value
-
-        # update texts
-        if p in textboxes:
-            textboxes[p].set_text(paramval)
+    def redraw():
+        params = order_initial_params(model, parameters)
+        y_line = model(x_plot, *params)
+        line.set_ydata(y_line)
 
         # Achsen automatisch anpassen
-        ymin = min(y_data.min(), y_plot.min())
-        ymax = max(y_data.max(), y_plot.max())
+        ymin = min(y_data.min(), y_line.min())
+        ymax = max(y_data.max(), y_line.max())
         dy = ymax - ymin or 1.0
         pad = 0.05 * dy
         ax.set_ylim(ymin - pad, ymax + pad)
 
-
-        # Canvas aktualisieren
-        fig.canvas.draw_idle()
-
-        _save_params(CACHE, param_values)
-
     for i, p in enumerate(params):
-        # UI-Positionen
-        y_pos = offset - (i+2) * sliderheight
-        slider_ax = plt.axes([0.15, y_pos, 0.35, sliderheight])
-        btn_minus_ax = plt.axes([0.75, y_pos, 0.05, 0.8 * sliderheight])
-        btn_plus_ax  = plt.axes([0.825, y_pos, 0.05, 0.8 * sliderheight])
-        # text_ax = plt.axes([0.505, y_pos + 0.005, 0.1, 0.8*sliderheight])
-
-         # Label for exponent
-        exp_ax = plt.axes([0.60, y_pos + 0.005, 0.04, 0.8*sliderheight])
-        exp_text = exp_ax.text(0.5, 0.5, fr"$\times 10^{{{slider_settings[p].exponent}}}$", ha='left', va='center')
-        exp_ax.axis('off')  # hide axes
-        exp_texts[p] = exp_text
-
-
-        # UI-Elemente
-        slider = Slider(slider_ax, p, 0.1, 12.0, valinit=slider_settings[p].slider_value)
-        btn_minus = Button(btn_minus_ax, '-')
-        btn_plus  = Button(btn_plus_ax, '+')
-        # text_box = TextBox(text_ax, "", color="white", hovercolor="lightgrey")
-        # text_box.text_disp.set_color("black")
-
-        # since garbage collector would remove; I NEED these references:
-        sliders[p] = slider
-        btns_plus[p] = btn_plus
-        btns_minus[p] = btn_minus
-        # textboxes[p] = text_box
-
-        # def make_update_textbox(param):
-        #     def update(text):
-        #         print("Du hast eingegeben:", text, "for", param)
-        #
-        #     return update;
-        #
-        # text_box.on_submit(make_update_textbox(p))
-
-        # Slider-Callback
-        def make_update(param):
-            def update(val, param=param):  # <-- fixiert param
-                slider_settings[param].slider_value = val
-                update_graph(param)
-            return update
-        slider.on_changed(make_update(p))
-
-        # Buttons-Callback
-        def make_exponent_buttons(param, slider, btn_plus, btn_minus):
-            def increase(event, param=param, slider=slider):
-                slider_settings[param].exponent += 1
-                slider.set_val(slider_settings[param].slider_value / 10)
-                exp_texts[param].set_text(fr"$\times 10^{{{slider_settings[p].exponent}}}$")
-                update_graph(param)
-
-            def decrease(event, param=param, slider=slider):
-                slider_settings[param].exponent -= 1
-                slider.set_val(slider_settings[param].slider_value * 10)
-                exp_texts[param].set_text(fr"$\times 10^{{{slider_settings[p].exponent}}}$")
-                update_graph(param)
-
-            btn_plus.on_clicked(increase)
-            btn_minus.on_clicked(decrease)
-
-        make_exponent_buttons(p, slider, btn_plus, btn_minus)
+        settings = cached[p] if p in cached else {};
+        parameters[p] = ParameterManager(
+            param = p,
+            total_ax = plt.axes((0.10, offset - (i+2)*sliderheight, 0.8, sliderheight)),
+            initial_value = settings.get("slider_value") or default_vals[i],
+            update_callback = redraw,
+            min = settings.get("min", None), 
+            max = settings.get("max", None), 
+            center = settings.get("center", None), 
+        )
 
     plt.show()
-    _save_params(CACHE, slider_settings)
 
-    initial_params = {}
-    for k, v in slider_settings.items():
-        initial_params[k] = v.value;
+    initial_parameters = {
+        p: v for p, v in zip(params, order_initial_params(model, parameters))
+    }
 
-    return initial_params
+    _save_slider_settings(CACHE, parameters)
+
+    return initial_parameters;
--- tests/graph_fit/test_find_init_params.py
+++ tests/graph_fit/test_find_init_params.py
@@ -1,8 +1,6 @@
-from dataclasses import asdict
-from batfloman_praktikum_lib.graph_fit.find_initial_parameters import InitialParameter, SliderSettings, order_initial_params
+from batfloman_praktikum_lib.graph_fit.find_initial_parameters import order_initial_params
 
 import pytest
-import random
 
 def test_order_initial_paramerters():
     def model1(x, A):
@@ -22,66 +20,3 @@ def test_order_initial_paramerters():
     with pytest.raises(ValueError):
         order_initial_params(model3, { "n": 0.5, "x": 1.0, "y": 2 })
 
-def test_initialParameters():
-    x = InitialParameter(value = 5)
-    assert asdict(x) == {"value": 5, "name": None}
-
-    x = InitialParameter(value = 5e19)
-    assert asdict(x) == {"value": 5e19, "name": None}
-
-    x = InitialParameter(value = 5, name="x0")
-    assert asdict(x) == {"value": 5, "name": "x0"}
-
-    x = InitialParameter(value = 5, name="x0")
-    assert InitialParameter.from_any(x) == x
-    assert InitialParameter.from_any(asdict(x)) == x
-
-    y = InitialParameter.from_any(x)
-    assert x == y
-
-    z = InitialParameter.from_any(asdict(x))
-    assert x == z
-
-    x = SliderSettings(
-        slider_value=1.0,
-        name="Test"
-    )
-    x = InitialParameter.from_any(x)
-    assert asdict(x) == {"value": 1, "name": "Test"}
-
-    x = SliderSettings(
-        slider_value=1.0,
-        exponent=1,
-        name="Test"
-    )
-    x = InitialParameter.from_any(x)
-    assert asdict(x) == {"value": 1e1, "name": "Test"}
-
-
-def test_sliderSettings():
-    for x in range(1000):
-        slider_val = random.random() * 1000 - 500
-        exponent = random.randint(-50, 50)
-        x = SliderSettings(
-            slider_value=slider_val,
-            exponent = exponent,
-        )
-        assert asdict(x) == {"name": None, "slider_value": slider_val, "exponent": exponent};
-        assert SliderSettings.from_any(x) == x
-        assert SliderSettings.from_any(asdict(x)) == x
-
-    for x in range(1000):
-        slider_val = random.random() * 1000 - 500
-        exponent = random.randint(-50, 50)
-        x = SliderSettings(
-            name = "test",
-            slider_value=slider_val,
-            exponent = exponent,
-        )
-        assert asdict(x) == {"name": "test", "slider_value": slider_val, "exponent": exponent};
-        assert SliderSettings.from_any(x) == x
-        assert SliderSettings.from_any(asdict(x)) == x
-
-
-    x = SliderSettings.from_any({"name": "A", "value": 5e9}) 
-    assert x == SliderSettings(name="A", slider_value = 5, exponent = 9)
