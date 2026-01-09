96bc775a5ca3375c5a04a86ba76da419b1536d59
Batfloman <flhirch@gmail.com>
2026-01-09 22:33:53 +0100

    auto decide on ls or ODR fit

--- src/batfloman_praktikum_lib/graph_fit/models/fitModel.py
+++ src/batfloman_praktikum_lib/graph_fit/models/fitModel.py
@@ -24,17 +24,18 @@ class FitModel(metaclass=ModelMeta):
 
     @classmethod
     def fit(cls, x, y, xerr = None, yerr = None) -> FitResult:
-        if all(hasattr(y, "value") and hasattr(y, "error") for y in y):
-            # Extract values and errors
-            yerr = np.array([y.error for y in y])
-            y = np.array([y.value for y in y])
+        # if all(hasattr(y, "value") and hasattr(y, "error") for y in y):
+        #     # Extract values and errors
+        #     yerr = np.array([y.error for y in y])
+        #     y = np.array([y.value for y in y])
+        #
+        # return cls.ls_fit(x, y, yerr=yerr);
 
-        return cls.ls_fit(x, y, yerr=yerr);
-        # has_xerr = xerr is not None and xerr.size > 0;
-        # if has_xerr:
-        #     return cls.odr_fit(x, y, xerr=xerr, yerr=yerr);
-        # else:
-        #     return cls.ls_fit(x, y, yerr=yerr);
+        has_xerr = xerr is not None and xerr.size > 0;
+        if has_xerr:
+            return cls.odr_fit(x, y, xerr=xerr, yerr=yerr);
+        else:
+            return cls.ls_fit(x, y, yerr=yerr);
     
     @classmethod
     def ls_fit(cls, x, y, yerr = None) -> FitResult:
@@ -48,7 +49,7 @@ class FitModel(metaclass=ModelMeta):
         param_names = cls.get_param_names()
         initial_guess = cls.get_initial_guess(x, y)
 
-        res = ls_fit(cls.model, x, y, yerr, initial_guess, param_names)
+        res = ls_fit(cls.model, x, y, yerr, initial_guess=initial_guess, param_names=param_names)
 
         # temporär Warnformat ändern
         old_format = warnings.formatwarning
@@ -69,7 +70,7 @@ class FitModel(metaclass=ModelMeta):
 
         param_names = cls.get_param_names()
         initial_guess = cls.get_initial_guess(x, y)
-        return odr_fit(cls.model, x, y, xerr=xerr, yerr=yerr, initial_guess=initial_guess, param_names=param_names)
+        return odr_fit(cls.model, x, y, x_err=xerr, y_err=yerr, initial_guess=initial_guess, param_names=param_names)
     
     @classmethod
     def on_data(cls, data: DataCluster, x_index: str, y_index: str) -> FitResult:
@@ -78,9 +79,7 @@ class FitModel(metaclass=ModelMeta):
         if not y_index in data.get_column_names():
             raise NameError(f"Column {y_index} not found")
 
-        x_values = data.values(x_index);
-        y_values = data.values(y_index);
-        x_errors = data.errors(x_index);
-        y_errors = data.errors(y_index);
+        x_values = data.column(x_index);
+        y_values = data.column(y_index);
 
-        return cls.fit(x_values, y_values, xerr=x_errors, yerr=y_errors);
+        return cls.fit(x_values, y_values);
